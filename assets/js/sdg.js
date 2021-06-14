a5894017fac0c11386ad10f984af6d3127b1eb47
LucyGwilliamAdmin <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2021-06-14 09:29:24 +0000

    Deploying to gh-pages from  @ a4e5e61a3164220c3c165e25dc092d02903887d6 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4454,47 +4454,29 @@ $(function() {
   L.Control.SearchAccessible = L.Control.Search.extend({
     onAdd: function(map) {
       var container = L.Control.Search.prototype.onAdd.call(this, map);
-      var listboxId = 'map-search-tooltip-list';
 
       this._input.setAttribute('aria-label', this._input.placeholder);
 
       this._button.setAttribute('role', 'button');
-      this._accessibleCollapseInput();
-      this._accessibleCollapseTooltip();
+      this._accessibleCollapse();
       this._button.innerHTML = '<i class="fa fa-search" aria-hidden="true"></i>';
 
       this._cancel.setAttribute('role', 'button');
       this._cancel.setAttribute('aria-label', this._cancel.title);
       this._cancel.innerHTML = '<i class="fa fa-close" aria-hidden="true"></i>';
 
-      this._tooltip.setAttribute('id', listboxId);
-      this._tooltip.setAttribute('role', 'listbox');
-
-      this._container.setAttribute('role', 'combobox');
-      this._container.setAttribute('aria-haspopup', 'listbox');
-      this._container.setAttribute('aria-owns', listboxId);
-
-      this._input.setAttribute('aria-autocomplete', 'list');
-      this._input.setAttribute('aria-controls', listboxId);
-
       // Prevent the delayed collapse when tabbing out of the input box.
       L.DomEvent.on(this._cancel, 'focus', this.collapseDelayedStop, this);
 
       return container;
     },
-    _accessibleExpandInput: function() {
+    _accessibleExpand: function() {
       this._accessibleDescription(translations.indicator.map_search_hide);
       this._button.setAttribute('aria-expanded', 'true');
     },
-    _accessibleCollapseInput: function() {
+    _accessibleCollapse: function() {
       this._accessibleDescription(translations.indicator.map_search_show);
       this._button.setAttribute('aria-expanded', 'false');
-    },
-    _accessibleExpandTooltip: function() {
-      this._container.setAttribute('aria-expanded', 'true');
-    },
-    _accessibleCollapseTooltip: function() {
-      this._container.setAttribute('aria-expanded', 'false');
     },
     _accessibleDescription: function(description) {
       this._button.title = description;
@@ -4502,24 +4484,23 @@ $(function() {
     },
     expand: function(toggle) {
       L.Control.Search.prototype.expand.call(this, toggle);
-      this._accessibleExpandInput();
+      this._accessibleExpand();
       return this;
     },
     collapse: function() {
       L.Control.Search.prototype.collapse.call(this);
-      this._accessibleCollapseInput();
+      this._accessibleCollapse();
       return this;
     },
     cancel: function() {
       L.Control.Search.prototype.cancel.call(this);
-      this._accessibleExpandInput();
+      this._accessibleExpand();
       return this;
     },
     showTooltip: function(records) {
       L.Control.Search.prototype.showTooltip.call(this, records);
       this._accessibleDescription(translations.indicator.map_search);
       this._button.removeAttribute('aria-expanded');
-      this._accessibleExpandTooltip();
       return this._countertips;
     },
     _handleSubmit: function(e) {
